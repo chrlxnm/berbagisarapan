0ccddd338bd96ebf81a1e4e0b8708ce6ab495c40
arya.andika2 <arya.andika2@bankmandiri.co.id>
2022-06-27 11:27:56 +0700

    login

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,9 +1,10 @@
-import { Input as AntdInput, Button, Form, Layout } from 'antd'
+import { Input as AntdInput, Button, Form, Layout,Col } from 'antd'
 
 import { ButtonWrapper } from '../../components/Wrapper';
 import Card from '../../components/Card';
 import React from 'react'
 import styled from 'styled-components';
+import "./index.css"
 
 const { Content } = Layout
 
@@ -17,30 +18,36 @@ function LoginPage() {
       
   return (
   <>
+  
   <Layout className={'content-homepage'}>
       <Content className="content-page">
-        <Card>
-            <h2>Login Page</h2>
-            <Form
-            {...layout}
-            form={form}
-            name="central-hooks"
-            autoComplete="off">
-              <Form.Item name={'username'} label='Username'>
-                  <Input />
-              </Form.Item>
-              <Form.Item name={'password'} label='Password'>
-                  <Input />
-              </Form.Item>
-              <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <ButtonWrapper>
-                <Button type="primary" htmlType="submit">
-                  Login
-                </Button>
-              </ButtonWrapper>
-              </Form.Item>
-            </Form>
-          </Card>
+      <Col style={{flexDirection:'row',display:'flex',justifyContent:'center', marginTop:window.innerHeight / 4}} xl={24} lg={24}>
+          <Col xl={8} lg={8}>
+          <Card style={{width:'50%'}}>
+              <h2>Login Page</h2>
+              <Form
+              {...layout}
+              form={form}
+              name="central-hooks"
+              autoComplete="off">
+                <Form.Item name={'username'} label='Username'>
+                    <Input />
+                </Form.Item>
+                <Form.Item name={'password'} label='Password'>
+                    <Input />
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <ButtonWrapper>
+                  <Button type="primary" htmlType="submit">
+                    Login
+                  </Button>
+                </ButtonWrapper>
+                </Form.Item>
+              </Form>
+            </Card>
+            </Col>
+            <Col></Col>
+            </Col>
         </Content>
       </Layout>
     </>
