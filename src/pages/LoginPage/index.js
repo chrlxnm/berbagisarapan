dd4242b81d9629be6b50e3d759b794c94153928c
arya.andika2 <arya.andika2@bankmandiri.co.id>
2022-06-27 13:33:29 +0700

    fix login

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,4 +1,4 @@
-import { Input as AntdInput, Button, Form, Layout,Col } from 'antd'
+import './style.css';
 
 import {
   Button as ButtonAntd,
@@ -12,54 +12,9 @@ import React, { Component } from "react";
 
 import logoCB from "../../assets/images/cb-logo.svg";
 import styled from 'styled-components';
-import "./index.css"
 
-const { Content } = Layout
-
-function LoginPage() {
-    const [form] = Form.useForm();
-
-    const layout = {
-        labelCol: { span: 8 },
-        wrapperCol: { span: 16 },
-      };
-      
-  return (
-  <>
-  
-  <Layout className={'content-homepage'}>
-      <Content className="content-page">
-      <Col style={{flexDirection:'row',display:'flex',justifyContent:'center', marginTop:window.innerHeight / 4}} xl={24} lg={24}>
-          <Col xl={8} lg={8}>
-          <Card style={{width:'50%'}}>
-              <h2>Login Page</h2>
-              <Form
-              {...layout}
-              form={form}
-              name="central-hooks"
-              autoComplete="off">
-                <Form.Item name={'username'} label='Username'>
-                    <Input />
-                </Form.Item>
-                <Form.Item name={'password'} label='Password'>
-                    <Input />
-                </Form.Item>
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <ButtonWrapper>
-                  <Button type="primary" htmlType="submit">
-                    Login
-                  </Button>
-                </ButtonWrapper>
-                </Form.Item>
-              </Form>
-            </Card>
-            </Col>
-            <Col></Col>
-            </Col>
-        </Content>
-      </Layout>
-    </>
-  )
+function onChange(checked) {
+  console.log(`switch to ${checked}`);
 }
 export default class Login extends Component {
   render() {
