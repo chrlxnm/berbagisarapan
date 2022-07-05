1ae8e50f9da6890046a33459beab32bd4f1ccb8d
CHOIRUL ANAM <choianam1987@gmail.com>
2022-07-05 23:48:16 +0700

    custom columns

---
+++ src/pages/MenuUtama/DaftarDonatur/Modal/columnList.js
@@ -0,0 +1,72 @@
+export const columns = () => {
+    let columns = [[{
+        value:'panggilan',
+        label:'Panggilan'
+        },
+        {
+        value:'nama',
+        label:'Nama'
+        },
+        {
+        value:'noWA',
+        label:'No WA'
+        }],[
+        {
+        value:'email',
+        label:'Email'
+        },
+        {
+        value:'agama',
+        label:'Agama'
+        },
+        {
+        value:'hobi',
+        label:'Hobi'
+        }],[
+        {
+        value:'kota',
+        label:'Kota'
+        },
+        {
+        value:'alamat',
+        label:'Alamat'
+        },
+        {
+        value:'jumlah',
+        label:'Berapa Kali'
+        }],[
+        {
+        value:'donasi',
+        label:'Total Donasi'
+        },
+        {
+        value:'date',
+        label:'Date Added'
+        },
+        {
+        value:'program',
+        label:'Program Favorit'
+        }],[
+        {
+        value:'kategori',
+        label:'Kategori'
+        },
+        {
+        value:'hasilSurvey',
+        label:'Hasil Survey'
+        },
+        {
+        value:'keterangan',
+        label:'Keterangan'
+        }],[
+        {
+        value:'admin',
+        label:'Admin'
+        },
+        {
+        value:'lastUpdate',
+        label:'Last Update'
+        }]
+    ]
+    return columns;
+}
